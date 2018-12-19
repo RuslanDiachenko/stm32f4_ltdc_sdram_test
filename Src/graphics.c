86016b57e801ffeb5783c6b47d393c20013dedc8
Ruslan Diachenko <ruslan.diachenko@lemberg.co.uk>
2018-12-19 18:31:39 +0200

    Successfully created test with 2 .png images

--- Src/graphics.c
+++ Src/graphics.c
@@ -1,5 +1,9 @@
 #include "GUI.h"
 #include "WM.h"
+#include "touch.h"
+
+extern GUI_CONST_STORAGE GUI_BITMAP bmtiger;
+extern GUI_CONST_STORAGE GUI_BITMAP bmfunnyCat;
 
 void DrawPlay(int x, int y, int size)
 {
@@ -15,6 +19,7 @@ GUI_AA_FillPolygon(pPoint, 3, x, y);
 
 void graphicsMain(void)
 {
+	/*
 	GUI_SetBkColor(GUI_DARKBLUE);
 	GUI_Clear();
 	GUI_SetDrawMode(GUI_DRAWMODE_NORMAL);
@@ -28,8 +33,10 @@ void graphicsMain(void)
 	GUI_TA_HCENTER, GUI_WRAPMODE_WORD);
 
 	GUI_SetColor(GUI_DARKMAGENTA);
-	DrawPlay(40, 220, 50);
-
+	DrawPlay(40, 220, 50);*/
+	GUI_Clear();
+	GUI_DrawBitmap(&bmfunnyCat, 0, 0);
+	uint8_t onChange = 0, pic = 0;
 	/*
 	GUI_SetColor(GUI_RED);
 	GUI_FillCircle(100,100,50);
@@ -41,9 +48,27 @@ void graphicsMain(void)
 	GUI_SetColor(GUI_ORANGE);
 	GUI_AA_FillCircle(200,200,40);
 	*/
+	tp_state_t tpState;
 	while (1)
 	{
-
-
+		TP_GetState(&tpState);
+		if (tpState.touchDetected)
+		{
+			onChange = 1;
+		}
+		if (onChange)
+		{
+			pic = !pic;
+			if (pic)
+			{
+				GUI_DrawBitmap(&bmfunnyCat, 0, 0);
+			}
+			else
+			{
+				GUI_DrawBitmap(&bmtiger, 0, 0);
+			}
+			onChange = 0;
+			GUI_Delay(40);
+		}
 	}
 }
